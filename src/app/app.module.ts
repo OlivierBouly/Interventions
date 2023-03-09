75cb2de401ace677900e513c7169bba9a0c59fc3
OLIVIER BOULY <olivier.bouly@ludicatif.com>
2023-03-09 14:13:13 -0500

    Travail 6 avancement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BienvenueComponent } from './bienvenue/bienvenue.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ProblemeComponent } from './probleme/probleme.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenueComponent,
+    AccueilComponent,
+    ProblemeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path:'accueil', component:AccueilComponent},
+      { path:'probleme', component:ProblemeComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
